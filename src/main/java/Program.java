022b04fde92e93994d952506da832580b7f24d4c
Yevhen Tyshchenko <tyschenko.evgeniy57@gmail.com>
2019-11-15 19:39:32 +0200

    HomeWork13

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,23 +1,205 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Program{
 
+    static final String url = "jdbc:mysql://localhost:3306/mysql?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static final String username = "root";
+    static final String password = "root";
+
+    static Connection conn;
+
     public static void main(String[] args) {
-        try{
-            String url = "jdbc:mysql://localhost:3306/mysql?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-            String username = "root";
-            String password = "root";
-            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            try (Connection conn = DriverManager.getConnection(url, username, password)){
+        Scanner sc = new Scanner(System.in);
+        try {
+            try {
+                conn = DriverManager.getConnection(url, username, password);
+                initDB();
 
-                System.out.println("Connection to Store DB succesfull!");
+                while (true) {
+                    System.out.println("1: add client");
+                    System.out.println("2: add random clients");
+                    System.out.println("3: delete client");
+                    System.out.println("4: change client");
+                    System.out.println("5: view clients");
+                    System.out.println("6: deleteParticulaClient");
+                    System.out.println("7: addClientPhone");
+                    System.out.print("-> ");
+
+                    String s = sc.nextLine();
+                    switch (s) {
+                        case "1":
+                            addClient(sc);
+                            break;
+                        case "2":
+                            insertRandomClients(sc);
+                            break;
+                        case "3":
+                            deleteClient(sc);
+                            break;
+                        case "4":
+                            changeClient(sc);
+                            break;
+                        case "5":
+                            viewClients();
+                            break;
+                        case "6":
+                            deleteParticulaClient(sc);
+                            break;
+                        case "7":
+                            addClientPhone(sc);
+                            break;
+                        default:
+                            return;
+                    }
+                }
+            } finally {
+                sc.close();
+                if (conn != null) conn.close();
             }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return;
         }
-        catch(Exception ex){
-            System.out.println("Connection failed...");
+    }
 
-            System.out.println(ex);
+
+    private static void initDB() throws SQLException {
+        Statement st = conn.createStatement();
+        try {
+            st.execute("DROP TABLE IF EXISTS Clients");
+            st.execute("CREATE TABLE Clients (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20) NOT NULL, age INT)");
+        } finally {
+            st.close();
+        }
+    }
+
+    private static void addClient(Scanner sc) throws SQLException {
+        System.out.print("Enter client name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter client age: ");
+        String sAge = sc.nextLine();
+        int age = Integer.parseInt(sAge);
+
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO Clients (name, age) VALUES(?, ?)");
+        try {
+            ps.setString(1, name);
+            ps.setInt(2, age);
+            ps.executeUpdate(); // for INSERT, UPDATE & DELETE
+        } finally {
+            ps.close();
+        }
+    }
+
+    private static void addClientPhone (Scanner sc) throws SQLException {
+        System.out.print("Enter client phone: ");
+        String phone = sc.nextLine();
+
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO Clients (phone) VALUES(?, ?)");
+        try {
+            ps.setString(1, phone);
+            ps.executeUpdate(); // for INSERT, UPDATE & DELETE
+        } finally {
+            ps.close();
+        }
+    }
+
+    private static void deleteClient(Scanner sc) throws SQLException {
+        System.out.print("Enter client name: ");
+        String name = sc.nextLine();
+
+        PreparedStatement ps = conn.prepareStatement("DELETE FROM Clients WHERE name = ?");
+        try {
+            ps.setString(1, name);
+            ps.executeUpdate(); // for INSERT, UPDATE & DELETE
+        } finally {
+            ps.close();
+        }
+    }
+
+    public static void deleteParticulaClient (Scanner sc) throws SQLException{
+        System.out.print("Enter client id: ");
+        String name = sc.nextLine();
+
+        PreparedStatement ps = conn.prepareStatement("DELETE FROM Clients WHERE id = 3");
+        try{
+            ps.setInt(1, 3);
+            ps.executeUpdate();
+        } finally {
+            ps.close();
+        }
+    }
+
+    private static void changeClient(Scanner sc) throws SQLException {
+        System.out.print("Enter client name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter new age: ");
+        String sAge = sc.nextLine();
+        int age = Integer.parseInt(sAge);
+
+        PreparedStatement ps = conn.prepareStatement("UPDATE Clients SET age = ? WHERE name = ?");
+        try {
+            ps.setInt(1, age);
+            ps.setString(2, name);
+            ps.executeUpdate(); // for INSERT, UPDATE & DELETE
+        } finally {
+            ps.close();
+        }
+    }
+
+    private static void insertRandomClients(Scanner sc) throws SQLException {
+        System.out.print("Enter clients count: ");
+        String sCount = sc.nextLine();
+        int count = Integer.parseInt(sCount);
+        Random rnd = new Random();
+
+        conn.setAutoCommit(false); // enable transactions
+        try {
+            try {
+                PreparedStatement ps = conn.prepareStatement("INSERT INTO Clients (name, age) VALUES(?, ?)");
+                try {
+                    for (int i = 0; i < count; i++) {
+                        ps.setString(1, "Name" + i);
+                        ps.setInt(2, rnd.nextInt(100));
+                        ps.executeUpdate();
+                    }
+                    conn.commit();
+                } finally {
+                    ps.close();
+                }
+            } catch (Exception ex) {
+                conn.rollback();
+            }
+        } finally {
+            conn.setAutoCommit(true); // return to default mode
+        }
+    }
+
+    private static void viewClients() throws SQLException {
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM Clients");
+        try {
+            // table of data representing a database result set,
+            ResultSet rs = ps.executeQuery();
+            try {
+                // can be used to get information about the types and properties of the columns in a ResultSet object
+                ResultSetMetaData md = rs.getMetaData();
+
+                for (int i = 1; i <= md.getColumnCount(); i++)
+                    System.out.print(md.getColumnName(i) + "\t\t");
+                System.out.println();
+
+                while (rs.next()) {
+                    for (int i = 1; i <= md.getColumnCount(); i++) {
+                        System.out.print(rs.getString(i) + "\t\t");
+                    }
+                    System.out.println();
+                }
+            } finally {
+                rs.close(); // rs can't be null according to the docs
+            }
+        } finally {
+            ps.close();
         }
     }
 }
